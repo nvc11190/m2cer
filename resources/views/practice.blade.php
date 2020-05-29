e65a8e1b0d0d21865f7b6bb0841e3654314b83d2
Nguyen Can <cannv@smartosc.com>
2020-05-29 11:24:15 +0700

    update

--- resources/views/practice.blade.php
+++ resources/views/practice.blade.php
@@ -145,7 +145,7 @@
             @endforeach
         </div>
         <div class="title">
-            @for ($i = 1; $i <= 389; $i++)
+            @for ($i = 1; $i <= 388; $i++)
                 @php
                     $class = '';
                     if ($i == 1) {
